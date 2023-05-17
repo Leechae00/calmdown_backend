6d8fef92b8dcf60bb0b6797f876816f1f3cd1696
Leechae00 <2codud0@gmail.com>
2023-05-18 00:56:30 +0900

    MedicineApiController PostMapping, GetMapping 작업

---
+++ src/main/java/com/example/calmdown/controller/MedicineApiController.java
@@ -0,0 +1,33 @@
+package com.example.calmdown.controller;
+
+import com.example.calmdown.dto.MedicineRequest;
+import com.example.calmdown.dto.MedicineResponse;
+import com.example.calmdown.dto.UserResponse;
+import com.example.calmdown.service.MedicineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class MedicineApiController {
+
+    private final MedicineService medicineService;
+
+    @PostMapping("/medicine")
+    public ResponseEntity<MedicineResponse> create(@RequestBody MedicineRequest medicineRequest) {
+        medicineService.create(medicineRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @GetMapping("/medicine")
+    public ResponseEntity<MedicineResponse> read(@RequestParam Long medicineID){
+        MedicineResponse medicineResponse = medicineService.read(medicineID);
+        return (medicineResponse != null)?
+                ResponseEntity.status(HttpStatus.OK).body(medicineResponse):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+
+    }
+
+}
