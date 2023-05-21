4a835201046669899f3502fc2e9618ff9b0a573b
Leechae00 <2codud0@gmail.com>
2023-05-21 15:16:28 +0900

    CalendarApiController 구현

---
+++ src/main/java/com/example/calmdown/controller/CalendarApiController.java
@@ -0,0 +1,32 @@
+package com.example.calmdown.controller;
+
+
+import com.example.calmdown.dto.CalendarRequest;
+import com.example.calmdown.dto.CalendarResponse;
+import com.example.calmdown.service.CalendarService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CalendarApiController {
+
+    private final CalendarService calendarService;
+
+    @PostMapping("/calendar")
+    public ResponseEntity<CalendarResponse> create(@RequestBody CalendarRequest calendarRequest){
+        calendarService.create(calendarRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+    @GetMapping("/calendar")
+    public ResponseEntity<CalendarResponse> read(@RequestParam Long userID){
+        CalendarResponse calendarResponse = calendarService.read(userID);
+        return (calendarResponse != null)?
+                ResponseEntity.status(HttpStatus.OK).body(calendarResponse):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+
+}
